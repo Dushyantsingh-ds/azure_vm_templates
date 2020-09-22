cfefe46f7be88ee3d2d3816db270e682ec6c24d0
Dushyant Singh <55306442+Dushyantsingh-ds@users.noreply.github.com>
2020-09-22 18:15:13 +0530

    Add files via upload

---
+++ HarshArenaAzureVMCreateScript.ps1
@@ -0,0 +1,7 @@
+﻿set-variable -name "USERNAME" -value "DUshyantSIngh"
+
+Set-Variable -name "PASSWORD" -value "Pa$$w0rd"
+
+New-AzResourceGroup -Name DushyantLabTest -Location EastUS2
+
+az vm create --name DSLabVM-1 --resource-group DushyantLabTest --image 'Win2016Datacenter' --size Standard_B1s --location EastUS2 --admin-username $USERNAME --admin-password $PASSWORD --zone 2
